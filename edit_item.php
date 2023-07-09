7ac1fa9d9f7ed685924a43d7ec82abb78d729885
leiny2002 <ven.bas14@gmail.com>
2023-07-09 15:50:51 +0800

    fixed Edit feature

--- edit_item.php
+++ edit_item.php
@@ -9,45 +9,34 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// Get the item ID from the request
-$itemId = $_GET['id'];
+// Check if the request is a POST request
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  // Get the submitted values from the form
+  $itemId = $_POST['id'];
+  $equipmentId = $_POST['equipment_id'];
+  $equipmentType = $_POST['equipment_type'];
+  $productModel = $_POST['product_model'];
+  $supplier = $_POST['supplier'];
+  $serialNumber = $_POST['serial_number'];
+  $endUser = $_POST['end_user'];
 
-// Prepare and execute the SQL statement to retrieve the item
-$stmt = $conn->prepare("SELECT * FROM technological_devices WHERE id = ?");
-$stmt->bind_param("i", $itemId);
-$stmt->execute();
-$result = $stmt->get_result();
+  // Prepare the update query
+  $stmt = $conn->prepare("UPDATE technological_devices SET equipment_id=?, equipment_type=?, product_model=?, supplier=?, serial_number=?, end_user=? WHERE id=?");
+  $stmt->bind_param("ssssssi", $equipmentId, $equipmentType, $productModel, $supplier, $serialNumber, $endUser, $itemId);
 
-$response = array(); // Initialize the response array
+  // Execute the update query
+  if ($stmt->execute()) {
+    header('Content-Type: text/plain');
+    echo 'success';
+  } else {
+    header('Content-Type: text/plain');
+    echo 'error';
+  }
 
-if ($result->num_rows > 0) {
-  // Item found, retrieve its details
-  $row = $result->fetch_assoc();
-
-  $item = array(
-    'id' => $row['id'],
-    'equipment_id' => $row['equipment_id'],
-    'equipment_type' => $row['equipment_type'],
-    'product_model' => $row['product_model'],
-    'supplier' => $row['supplier'],
-    'serial_number' => $row['serial_number'],
-    'end_user' => $row['end_user']
-  );
-
-  $response['status'] = 'success';
-  $response['data'] = $item;
-} else {
-  // Item not found
-  $response['status'] = 'error';
-  $response['message'] = 'Item not found.';
+  // Close the prepared statement
+  $stmt->close();
 }
 
-$stmt->close();
+// Close the database connection
 $conn->close();
-
-// Set the appropriate content type for JSON response
-header('Content-Type: application/json');
-
-// Encode the response array as JSON and send the response
-echo json_encode($response);
 ?>
